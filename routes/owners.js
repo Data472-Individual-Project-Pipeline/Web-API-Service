436d275b487d6bb9b4fdab711adc3389cba55f43
Hua <hwa205@uclive.ac.nz>
2024-05-20 07:19:02 +0000

    remove nodejs files

--- routes/owners.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const OwnerController = require('../controllers/OwnerController');
-
-router.get('/', OwnerController.getAllOwners);
-
-module.exports = router;
